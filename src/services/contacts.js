0950fd28ff54e598e1c2012300f3bc3eddb400e7
lilianantw <liliana.ntw@gmail.com>
2025-09-12 14:13:21 +0300

    Добавил проверку POST/GET для контактов

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,11 +1,13 @@
-import { ContactsCollection } from "../db/models/contacts";
+import { ContactsCollection } from "../db/models/contacts.js";
 
+
+//получить все контакты 
 export const getAllContacts =  async () => {
     const contacts =  await ContactsCollection.find();
     return contacts;
 } 
-
-export const getContactsById = async () => {
+//получить контакт по ID
+export const getContactsById = async (contactId) => {
     const contact =  await ContactsCollection.findById(contactId);
     return contact;
 };
