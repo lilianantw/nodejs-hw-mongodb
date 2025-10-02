5ce6b17b3f884d72907533495bc92a5b98a3fbc8
lilianantw <liliana.ntw@gmail.com>
2025-10-03 00:54:09 +0300

    Прибрала обмеження мін/макс довжини для name у валідації

--- src/validation/auth.js
+++ src/validation/auth.js
@@ -3,8 +3,8 @@
 import Joi from "joi";
 
 export const registerUserSchema = Joi.object({
-name: Joi.string().min(3).max(20).required(),
-email: Joi.string().email().min(3).max(20).trim().lowercase().required(),
+name: Joi.string().required(),
+email: Joi.string().email().trim().lowercase().required(),
 password: Joi.string().required()
 });
 
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,18 +4,18 @@ import Joi from "joi";
 
 
 export const createContactsSchema = Joi.object({
-    name: Joi.string().min(3).max(20).required(),
-    phoneNumber: Joi.string().min(3).max(20).required(),
-    email: Joi.string().email().min(3).max(20),
+    name: Joi.string().required(),
+    phoneNumber: Joi.string().required(),
+    email: Joi.string().email(),
     isFavourite: Joi.boolean().default(false),
-    contactType: Joi.string().min(3).max(20).valid("work", "home", "personal").default("personal").required(),
+    contactType: Joi.string().valid("work", "home", "personal").default("personal").required(),
 });
 
 
 export const updateContactsSchema = Joi.object({
-name: Joi.string().min(3).max(20),
-phoneNumber: Joi.string().min(3).max(20),
-email: Joi.string().email().min(3).max(20),
+name: Joi.string(),
+phoneNumber: Joi.string(),
+email: Joi.string().email(),
 isFavourite: Joi.boolean(),
-contactType: Joi.string().min(3).max(20).valid("work", "home", "personal"),
+contactType: Joi.string().valid("work", "home", "personal"),
 });
